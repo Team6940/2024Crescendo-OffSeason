773dcce9fb6e71676d6bb30dc6ff63355a9c0d5c
XM-71-HOPE <2481482242@qq.com>
2024-08-07 18:12:27 +0800

    Manual Shoot Added

--- 2024Crescendo-OffSeason/src/main/java/frc/robot/commands/ManualSPKUp.java
+++ 2024Crescendo-OffSeason/src/main/java/frc/robot/commands/ManualSPKUp.java
@@ -5,25 +5,53 @@ import frc.robot.RobotContainer;
 import frc.robot.Constants.ArmConstants;
 import frc.robot.Constants.BlockerConstants;
 import frc.robot.Constants.ShooterConstants;
+import frc.robot.Constants.ManualShootConstants;
 
 /** An example command that uses an example subsystem. */
 public class ManualSPKUp extends Command {
   private int m_ButtonID;
-  public ManualSPKUp(int _ButtonID){
+  private double m_ArmAngle = 0.;
+  private double m_RPS = 0.;
+  private ShootState m_State;
+  public ManualSPKUp(int _ButtonID, int _ArmAngle, int _RPS){
     m_ButtonID=_ButtonID;
+    m_ArmAngle=_ArmAngle;
+    m_RPS=_RPS;
     addRequirements(RobotContainer.m_Shooter);
     addRequirements(RobotContainer.m_Arm);
-    }
+    addRequirements(RobotContainer.m_Blocker);
+  }
+  public enum ShootState{
+    Preparing, Shooting;
+  }
   @Override
   public void initialize() {
-    RobotContainer.m_Arm.SetArmDegree(ArmConstants.ArmUpSPKDegree);
-    RobotContainer.m_Shooter.SetRPS(ShooterConstants.ShooterManualSPKRPS);
+    m_State=ShootState.Preparing;
   }
 
   @Override
   public void execute() {
-    if(RobotContainer.m_Shooter.IsAtTargetRPS()&&RobotContainer.m_Arm.IsAtTargetDegree())
-      RobotContainer.m_Blocker.SetOutPut(BlockerConstants.GiveNoteOutput);
+    switch(m_State){
+      case Preparing:
+        prepare();
+        break;
+      case Shooting:
+        shoot();
+        break;
+      default:
+        break;
+    }
+  }
+
+  private void prepare(){
+    RobotContainer.m_Arm.SetArmDegree(m_ArmAngle);
+    RobotContainer.m_Shooter.SetRPS(m_RPS);
+    if(RobotContainer.m_Arm.IsAtTargetDegree() && RobotContainer.m_Shooter.IsAtTargetRPS() && RobotContainer.m_Swerve.getChassisSpeed() <= ManualShootConstants.ChassisSpeedTolerance){
+      m_State = ShootState.Shooting;
+    }
+  }
+  private void shoot(){
+    RobotContainer.m_Blocker.SetOutPut(BlockerConstants.GiveNoteOutput);
   }
 
   @Override
@@ -34,7 +62,7 @@ public class ManualSPKUp extends Command {
 
   @Override
   public boolean isFinished() {
-    if(!RobotContainer.m_driverController.getButton(m_ButtonID)) return true;
-    else return false;
+    if(RobotContainer.m_driverController.getRawButton(m_ButtonID)) return false;
+    return true;
   }
 }
